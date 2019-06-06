5368c78638778dacaa0d2ea07894df0b197db6ea
Matthew Abrams <mcabrams1@gmail.com>
2019-06-06 13:30:07 -0700

    Implement links; refactor schema into separate file

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,22 +1,41 @@
+import { importSchema } from 'graphql-import'
+import { makeExecutableSchema } from 'graphql-tools'
 import express from 'express';
 import { ApolloServer, gql } from 'apollo-server-express';
 import compression from 'compression';
 
-// Construct a schema, using GraphQL schema language
-const typeDefs = gql`
-  type Query {
-    hello: String
-  }
-`;
+let links = [{
+  id: 'link-0',
+  url: 'foo.com',
+  description: 'hi',
+}];
+
+type Link = {
+  id: string;
+  description: string;
+  url: string;
+}
+
+const typeDefs = importSchema(__dirname + '/schema.graphql');
 
 // Provide resolver functions for your schema fields
 const resolvers = {
   Query: {
-    hello: () => 'Hello world!',
+    hello: () => 'hi!',
+    feed: () => links,
   },
+  Link: {
+    id: (parent: Link) => parent.id,
+    description: (parent: Link) => parent.description,
+    url: (parent: Link) => parent.url,
+  }
 };
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const schema = makeExecutableSchema({ typeDefs, resolvers })
+
+const server = new ApolloServer({
+  schema,
+});
 
 const app = express();
 server.applyMiddleware({ app });
