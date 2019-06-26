4e112ee743fee762fb963c31c5e292409879e82f
Matthew Abrams <mcabrams1@gmail.com>
2019-06-25 23:00:56 -0700

    Add feed filtering

--- server/src/resolvers/Query.ts
+++ server/src/resolvers/Query.ts
@@ -1,8 +1,20 @@
 import { Context } from '@src/types';
 import { LinkWhereUniqueInput } from '@src/generated/prisma-client';
 
-export const feed = (root: any, args: null, context: Context) =>
-  context.prisma.links();
+export const feed = async (
+  root: any, args: {filter?: string;}, context: Context,
+) => {
+  const where = args.filter ? {
+    OR: [
+      {description_contains: args.filter},
+      {url_contains: args.filter},
+    ],
+  } : {};
+  const links = await context.prisma.links({
+    where,
+  });
+  return links;
+}
 
 export const link = (
   parent: null, args: LinkWhereUniqueInput, context: Context,
